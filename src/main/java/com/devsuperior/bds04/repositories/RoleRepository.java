5f3063c5a6a947558da3fe1f492f994c952723de
ValberMedeiros <dev.junior.medeiros@gmail.com>
2021-06-24 21:49:02 -0300

    Repositórios.

---
+++ src/main/java/com/devsuperior/bds04/repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.devsuperior.bds04.repositories;
+
+import com.devsuperior.bds04.dto.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
