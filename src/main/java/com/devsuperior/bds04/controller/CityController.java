2bb5649a2ed1e10bc4e7662468cc360db6d3e4e5
ValberMedeiros <dev.junior.medeiros@gmail.com>
2021-06-24 22:50:53 -0300

    City Controller.

---
+++ src/main/java/com/devsuperior/bds04/controller/CityController.java
@@ -0,0 +1,40 @@
+package com.devsuperior.bds04.controller;
+
+import com.devsuperior.bds04.dto.CityDTO;
+import com.devsuperior.bds04.services.CityService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/cities")
+public class CityController {
+
+    private CityService service;
+
+    public CityController(CityService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CityDTO>> findAll() {
+        List<CityDTO> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<CityDTO> insert(@RequestBody @Valid CityDTO dto) {
+        dto = service.insert(dto);
+        var uri = ServletUriComponentsBuilder
+                .fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+}
