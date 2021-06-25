b457537e7110aac4a60b39f259b226e3446dec2d
ValberMedeiros <dev.junior.medeiros@gmail.com>
2021-06-24 22:13:38 -0300

    Exceptions.

---
+++ src/main/java/com/devsuperior/bds04/controller/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,56 @@
+package com.devsuperior.bds04.controller.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFoundHandler(ResourceNotFoundException ex, HttpServletRequest request) {
+        var status = HttpStatus.NOT_FOUND;
+        var error = getStandardError(ex, status, request, "Resource not found");
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> methodArgumentNotValidException(MethodArgumentNotValidException ex
+            , HttpServletRequest request) {
+        var status = HttpStatus.UNPROCESSABLE_ENTITY;
+        var error = getValidationError(ex, status, request, "Validation exception");
+        return ResponseEntity.status(status).body(error);
+    }
+
+    private StandardError getStandardError(Exception ex, HttpStatus status,
+                                           HttpServletRequest request, String errorMsg) {
+        var error = new StandardError();
+        error.setTimestamp(Instant.now());
+        error.setStatus(status.value());
+        error.setError(errorMsg);
+        error.setMessage(ex.getMessage());
+        error.setPath(request.getRequestURI());
+        return error;
+    }
+
+    private ValidationError getValidationError(MethodArgumentNotValidException ex, HttpStatus status,
+                                               HttpServletRequest request, String errorMsg) {
+        var error = new ValidationError();
+        error.setTimestamp(Instant.now());
+        error.setStatus(status.value());
+        error.setError(errorMsg);
+        error.setMessage(ex.getMessage());
+        error.setPath(request.getRequestURI());
+        for (FieldError field : ex.getBindingResult().getFieldErrors()) {
+            error.addError(field.getField(), field.getDefaultMessage());
+        }
+        return error;
+    }
+
+}
